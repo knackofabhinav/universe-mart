b5b78c1a43b3069ec9e1f615acdfe0ca370ae712
Abhinav Niranjan <66178038+knackofabhinav@users.noreply.github.com>
2021-03-18 08:13:40 +0530

    project initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import {useTheme} from '../src/contexts/theme-context' 
+import {Navigation} from './components/Navigation/Navigation'
+import {ProductListing} from './components/Product Listing/ProductListing'
+import {Wishlist} from './components/Wishlist/Wishlist'
+import {Cart} from './components/Cart/Cart'
+const axios = require('axios');
+
 
 function App() {
+  const {theme:{backgroundColor, color}} = useTheme();
+  const [route, setRoute] = useState('products');
+
+  useEffect(() => {
+    (async function(){
+      try{
+        const {data} = await axios.get("/api/productlist")
+        console.log(data.productlist)
+      }
+      catch (error){
+        console.error(error)
+      }
+      finally{
+        console.log("done")
+      }
+    })();
+  }, [])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor: backgroundColor, color: color}}>
+      <Navigation setRoute={setRoute}/>
+      {route==='product' && <ProductListing />}
+      {route==='wishlist' && <Wishlist />}
+      {route==='cart' && <Cart />}
     </div>
   );
 }
